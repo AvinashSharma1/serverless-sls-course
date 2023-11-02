1bd20ead2096a7df8cc4c0e751f8a08c96cba387
Avinash Sharma <Avinash.Sharma@leadventure.com>
2023-11-02 09:05:39 +0530

    Dynamo DB table create in yaml - IaC

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,15 @@
 async function createAuction(event, context) {
+  const {title} = JSON.parse(event.body);
+  const now = new Date();
+  const auction = {
+    title,
+    status:"OPEN",
+    createdAT: now.toISOString()
+  };
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    statusCode: 201,
+    body: JSON.stringify({auction})
   };
 }
 
