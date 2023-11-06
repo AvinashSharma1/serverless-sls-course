e49735be0928211f5d3dfc993cb74322d0409643
Avinash Sharma <avinash.sharma.cet@gmail.com>
2023-11-06 17:20:06 +0530

    Get auctions and get auction by id as well

---
+++ src/handlers/getAuctions.js
@@ -0,0 +1,32 @@
+import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+async function getAuctions(event, context){
+  let auctions;
+  try {
+    const result = await dynamoDB.scan({TableName: process.env.AUCTIONS_TABLE_NAME}).promise();
+    auctions = result.Items;
+    
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+    
+  }  
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({auctions})
+  };
+}
+
+export const handler = middy(getAuctions)
+.use(httpJsonBodyParser())
+.use(httpEventNormalizer())
+.use(httpErrorHandler());
+
+
